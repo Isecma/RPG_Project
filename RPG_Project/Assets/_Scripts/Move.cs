8a6efd9ce71d3fcce5d10579cb5fb8a9fff99a60
Ismael Echegaray <ismael.echegarayms@hotmail.com>
2022-10-14 13:19:43 -0500

    Dummy Scene
    -Add a Dummy Scene to play with the NavMesh
    -Using Raycast so the player can click to move

---
+++ RPG_Project/Assets/_Scripts/Move.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }   
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        
+    }
+}
