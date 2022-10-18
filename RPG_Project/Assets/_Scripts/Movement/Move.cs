c6e1aa05cd67ee8d8f52fc8e7704d6d5b4753f09
Ismael Echegaray <ismael.echegarayms@hotmail.com>
2022-10-18 12:31:12 -0500

    Follow Camera and Animation
    -Add fixed follow camera logic
    -Import animations for idle, walk and run
    -Import assets for characters and environment
    -Add blend tree for forward movement

---
+++ RPG_Project/Assets/_Scripts/Movement/Move.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        
+    }
+
+    void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+}
